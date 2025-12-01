41d8c9d1b654eefe8a4bb319ffeb88e819e5cbf0
Jay Bhingradiya <jaybhingradiya7@gmail.com>
2025-12-01 14:41:36 +0530

    fix: Change default theme in Button component from secondary to tertiary

--- lib/components/button/index.tsx
+++ lib/components/button/index.tsx
@@ -8,11 +8,7 @@ const themeClasses = {
   secondary: "bg-secondary hover:bg-secondary-dark",
   tertiary: "bg-tertiary hover:bg-tertiary-dark",
 };
-const Button: React.FC<ButtonProps> = ({
-  label,
-  theme = "secondary",
-  icon,
-}) => {
+const Button: React.FC<ButtonProps> = ({ label, theme = "tertiary", icon }) => {
   return (
     <button
       className={`${themeClasses[theme]} text-white px-[8px] py-[10px] rounded-lg flex flex-row gap-2 justify-center items-center`}
