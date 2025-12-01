4b716c5abe28538332b1c5bd547dd76dfca81d47
Jay Bhingradiya <jaybhingradiya7@gmail.com>
2025-12-01 14:25:54 +0530

    fix: Update default theme in Button component and format code

--- lib/components/button/index.tsx
+++ lib/components/button/index.tsx
@@ -8,10 +8,15 @@ const themeClasses = {
   secondary: "bg-secondary hover:bg-secondary-dark",
   tertiary: "bg-tertiary hover:bg-tertiary-dark",
 };
-const Button: React.FC<ButtonProps> = ({ label, theme = "primary", icon }) => {
+const Button: React.FC<ButtonProps> = ({
+  label,
+  theme = "secondary",
+  icon,
+}) => {
   return (
     <button
-      className={`${themeClasses[theme]} text-white px-[8px] py-[10px] rounded-lg flex flex-row gap-2 justify-center items-center`}>
+      className={`${themeClasses[theme]} text-white px-[8px] py-[10px] rounded-lg flex flex-row gap-2 justify-center items-center`}
+    >
       {icon && <span className="icon text-center">{icon}</span>}
 
       {label}
